b97f2d1ce11b17d592b95207ef2177aae3843be7
Dabsia <dabojohnson98@gmail.com>
2022-01-29 18:28:30 +0000

    Commented my code for readability

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import   './collection-item.styles.scss';
 
+// Takes in props from the shop component and creates a template so it can be exported to the collection preview component
+
 const CollectionItem = ({name,price,imageUrl}) => {
     return (
         <div className='collection-item'>
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,11 +7,13 @@ const CollectionPreview = ({title,items }) => {
         <div className='collection-preview'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <div className='preview'>
-        {
-            items.filter((item,idx) => idx < 4).map(({id, ...otherItemProps }) => (
-                <CollectionItem key = {id} {...otherItemProps}/>
-            ))
-        }
+                // This component displays for items from our storeData file 
+                {/* After looping it displays the collenction  */}
+                {
+                    items.filter((item,idx) => idx < 4).map(({id, ...otherItemProps }) => (
+                        <CollectionItem key = {id} {...otherItemProps}/>
+                    ))
+                }
             </div>
         </div>
     )
